78215e9076c269c3efbeecb7f5fc5ace08ffbbbd
abdelkayoum <abdelkayoumkh0@gmail.com>
2026-02-05 23:02:38 +0100

    add  row solidstar

--- lib/Features/home/presentation/views/widget/best_seller_list_views.dart
+++ lib/Features/home/presentation/views/widget/best_seller_list_views.dart
@@ -2,6 +2,7 @@ import 'package:book/Features/home/utile/style.dart';
 import 'package:book/constant.dart';
 import 'package:book/constant_images.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ListTileBest extends StatefulWidget {
   const ListTileBest({super.key});
@@ -37,66 +38,73 @@ class _ListTileBestState extends State<ListTileBest> {
               ),
             ),
             SizedBox(width: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    'Harry Potter and the Goblet of FireHarry Potter and the GobletGoblet',
-                    maxLines: isisvisible ? null : 2,
-                    overflow: isisvisible
-                        ? TextOverflow.visible
-                        : TextOverflow.ellipsis,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the Goblet of FireHarry Potter and the GobletGoblet',
+                      maxLines: isisvisible ? null : 2,
+                      overflow: isisvisible
+                          ? TextOverflow.visible
+                          : TextOverflow.ellipsis,
 
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kGtSectraFine,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isisvisible = !isisvisible;
-                    });
-                  },
-                  child: isisvisible
-                      ? Text(
-                          'less More',
-                          style: isisvisible
-                              ? TextStyle(color: Colors.red)
-                              : null,
-                        )
-                      : Text(
-                          'Show More',
-                          style: isisvisible
-                              ? null
-                              : TextStyle(color: Colors.greenAccent),
-                        ),
-                ),
-
-                Text('J.K Rowling', style: Styles.textStyle14),
-                Row(
-                  children: [
-                    Text(
-                      '${19.99}',
                       style: Styles.textStyle20.copyWith(
-                        fontWeight: FontWeight.bold,
+                        fontFamily: kGtSectraFine,
                       ),
                     ),
-                    SizedBox(width: 10),
-                    Row(
-                      children: [
-                        Text('${4.8} ', style: Styles.textStyle18),
-                        Text(
-                          ' ${(2390)}',
-                          style: TextStyle(color: Colors.grey.withValues()),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isisvisible = !isisvisible;
+                      });
+                    },
+                    child: isisvisible
+                        ? Text(
+                            'less More',
+                            style: isisvisible
+                                ? TextStyle(color: Colors.red)
+                                : null,
+                          )
+                        : Text(
+                            'Show More',
+                            style: isisvisible
+                                ? null
+                                : TextStyle(color: Colors.greenAccent),
+                          ),
+                  ),
+
+                  Text('J.K Rowling', style: Styles.textStyle14),
+                  Row(
+                    children: [
+                      Text(
+                        '${19.99}',
+                        style: Styles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                      ),
+                      Expanded(child: SizedBox()),
+                      Row(
+                        children: [
+                          Icon(FontAwesomeIcons.solidStar, color: Colors.amber),
+                          SizedBox(width: 6.3),
+                          Text('${4.8} ', style: Styles.textStyle16),
+                          SizedBox(width: 5),
+                          Text(
+                            ' (2390)',
+                            style: Styles.textStyle14.copyWith(
+                              color: Color(0xff707070),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         );

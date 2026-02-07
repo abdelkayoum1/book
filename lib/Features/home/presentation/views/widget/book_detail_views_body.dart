531acc678ac0becb9795f2bc0c36372af6284610
abdelkayoum <abdelkayoumkh0@gmail.com>
2026-02-07 22:55:35 +0100

    add CostumerscroolViews

--- lib/Features/home/presentation/views/widget/book_detail_views_body.dart
+++ lib/Features/home/presentation/views/widget/book_detail_views_body.dart
@@ -15,60 +15,67 @@ class BookDetailViewsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8.0,
-                vertical: 15,
-              ),
-              child: BookdetailviewsAppbar(),
-            ),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * .35),
-              child: CusomterListViewsImage(),
-            ),
-            SizedBox(height: 43),
-            Text('The Jungle Book', style: Styles.textStyle14),
-
-            Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
               children: [
-                Icon(FontAwesomeIcons.solidStar, color: Colors.amber),
-                SizedBox(width: 6.3),
-                Text('${4.8} ', style: Styles.textStyle16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                    vertical: 15,
+                  ),
+                  child: BookdetailviewsAppbar(),
+                ),
 
-                SizedBox(height: 5),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .35),
+                  child: CusomterListViewsImage(),
+                ),
+                SizedBox(height: 43),
+                Text('The Jungle Book', style: Styles.textStyle14),
+
                 Text(
-                  ' (2390)',
+                  'Rudyard Kipling',
                   style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(FontAwesomeIcons.solidStar, color: Colors.amber),
+                    SizedBox(width: 6.3),
+                    Text('${4.8} ', style: Styles.textStyle16),
+
+                    SizedBox(height: 5),
+                    Text(
+                      ' (2390)',
+                      style: Styles.textStyle14.copyWith(
+                        color: Color(0xff707070),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                BookActions(),
+                Expanded(child: SizedBox(height: 50)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('You can also like'),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: BookListViewsdetail__like(),
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            BookActions(),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('You can also like'),
-              ),
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: BookListViewsdetail__like(),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

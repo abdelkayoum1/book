8d4cb2b3b6213da50907d9f71d920df5613ed728
abdelkayoum <abdelkayoumkh0@gmail.com>
2026-02-09 17:56:23 +0100

    ceate feature_newset_book_cubit

--- lib/Features/home/presentation/manager/feature_newset_book_cubit/feature_newset_book_cubit.dart
+++ lib/Features/home/presentation/manager/feature_newset_book_cubit/feature_newset_book_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:book/Features/home/data/model/book_model/book_model.dart';
+import 'package:book/Features/home/data/repos/home_repos.dart';
 import 'package:equatable/equatable.dart';
 
 part 'feature_newset_book_state.dart';
 
 class FeatureNewsetBookCubit extends Cubit<FeatureNewsetBookState> {
-  FeatureNewsetBookCubit() : super(FeatureNewsetBookInitial());
+  FeatureNewsetBookCubit(this.homerepo) : super(FeatureNewsetBookInitial());
+
+  final HomeRepos homerepo;
+
+  Future<void> Feature_newset_book() async {
+    emit(FeatureNewsetBookloading());
+    var resultat = await homerepo.fetcheNewestBooks();
+
+    resultat.fold(
+      (failure) {
+        emit(FeatureNewsetBookfailure(failure.error.toString()));
+      },
+      (books) {
+        emit(FeatureNewsetBooksucces(books));
+      },
+    );
+  }
 }
